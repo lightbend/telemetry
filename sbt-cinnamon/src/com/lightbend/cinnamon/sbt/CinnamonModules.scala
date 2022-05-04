4fd3b78b0b14e9768e381e2483d7dae348b0cafa
Justin Pihony <justin.pihony@gmail.com>
2022-05-04 11:02:35 -0400

    sbt-cinnamon 2.16.3

--- sbt-cinnamon/src/com/lightbend/cinnamon/sbt/CinnamonModules.scala
+++ sbt-cinnamon/src/com/lightbend/cinnamon/sbt/CinnamonModules.scala
@@ -4,7 +4,7 @@ import sbt._
 
 object CinnamonLibrary extends Dynamic {
   val cinnamonOrganization: String = "com.lightbend.cinnamon"
-  val cinnamonVersion: String = "2.16.2"
+  val cinnamonVersion: String = "2.16.3"
 
   def apply(name: String, cross: Boolean = true): ModuleID = {
     cinnamonOrganization % name % cinnamonVersion cross SbtCrossVersion(cross)
